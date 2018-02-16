12f7b93f410806d5e00f50a52076af69fa9aafa7
Velibor Savanović <velibor_sav@live.com>
2018-02-16 15:10:54 +0100

    first commit.

---
+++ webroot/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JAVA PRACTICE</title>
+    <link rel="stylesheet" href="styles/java.css">
+</head>
+<body>
+<div class="container">
+<h1>MEMORY GAMES</h1>
+<h2>PLAY GAME</h2>
+    <hr>
+    <ul class="game_grid" id="grid">
+        <li id="tresnja" class="slide "><img class="slika1" src="img/Cherry.png" alt=""></li>
+        <li id="tresnja" class="slide  "><img class="slika1" src="img/Cherry.png" alt=""></li>
+        <li id="lubenica" class="slide"><img class="slika1" src="img/Watermelon.png" alt=""></li>
+        <li id="lubenica" class="slide"><img class="slika1" src="img/Watermelon.png" alt=""></li>
+        <li id="sljiva" class="slide"><img class="slika1" src="img/Plum.png" alt=""></li>
+        <li id="sljiva" class="slide"><img class="slika1" src="img/Plum.png" alt=""></li>
+        <li id="zvono" class="slide"><img class="slika1" src="img/casino_bell.png" alt=""></li>
+        <li id="zvono" class="slide"><img class="slika1" src="img/casino_bell.png" alt=""></li>
+        <li id="limun" class="slide"><img class="slika1" src="img/Lemon-512.png" alt=""></li>
+        <li id="limun" class="slide"><img class="slika1" src="img/Lemon-512.png" alt=""></li>
+        <li id="sedam" class="slide"><img class="slika1" src="img/Lucky_Seven-512.png" alt=""></li>
+        <li id="sedam" class="slide"><img class="slika1" src="img/Lucky_Seven-512.png" alt=""></li>
+        <li id="drupal" class="slide"><img class="slika1" src="img/drupal.png" alt=""></li>
+        <li id="drupal" class="slide"><img class="slika1" src="img/drupal.png" alt=""></li>
+        <li id="sublime" class="slide"><img class="slika1" src="img/sublime.jpg" alt=""></li>
+        <li id="sublime" class="slide"><img class="slika1" src="img/sublime.jpg" alt=""></li>
+    </ul>
+
+    <button id="btn" class="shuffling">SHUFFLING!</button>
+</div>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+<script src="jquery/jquery.js"></script>
+
+</body>
+</html>
